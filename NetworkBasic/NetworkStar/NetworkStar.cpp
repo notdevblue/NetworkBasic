c100379445afa79622482bf13e43ae37ba2d2374
Han <wooyeophan@gmail.com>
2021-09-03 15:23:32 +0900

    네트워크 시간 별찍기 하는 중

---
+++ NetworkBasic/NetworkStar/NetworkStar.cpp
@@ -0,0 +1,103 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main()
+{
+	int height = 10;
+
+	// 역피라미드
+	std::cout << "역피라미드\r\n";
+	for (int i = 0; i < height; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			std::cout << " ";
+		}
+
+		for (int j = 0; j < height * 2 - i * 2 - 1; ++j)
+		{
+			std::cout << "*";
+		}
+
+		std::cout << "\r\n";
+	}
+
+
+	std::cout << "\r\n";
+	std::cout << "\r\n";
+
+	// 속이 빈 마름모
+	std::cout << "속이 빈 마름모\r\n";
+	for (int i = 0; i < height; ++i)
+	{
+		for (int j = 0; j < height - i - 1; ++j)
+		{
+			std::cout << " ";
+		}
+		
+		if (i != height - 1)
+		{
+			for (int j = 0; j <= i * 2; ++j)
+			{
+				if (j == 0 || j == i * 2)
+				{
+					std::cout << "*";
+				}
+				else
+				{
+					std::cout << " ";
+				}
+			}
+
+			std::cout << "\r\n";
+		}
+	}
+
+	for (int i = 0; i < height; ++i)
+	{
+		for (int j = 0; j < i; ++j)
+		{
+			std::cout << " ";
+		}
+
+		for (int j = 0; j < height * 2 - i * 2 - 1; ++j)
+		{
+			if (j == 0 || j == height * 2 - i * 2 - 2)
+			{
+				std::cout << "*";
+			}
+			else
+			{
+				std::cout << " ";
+			}
+		}
+
+		std::cout << "\r\n";
+
+	}
+	
+	std::cout << "\r\n";
+
+	std::cout << "원\r\n";
+	int r = 10;
+
+	for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < r; ++j)
+		{
+			if(pow(i - r / 2, 2) + pow(j - r / 2, 2) < pow(r / 2 , 2))
+			{
+				std::cout << "*";
+			}
+			else
+			{
+				std::cout << " ";
+			}
+		}
+
+		std::cout << "\r\n";
+	}
+
+
+	return(0);
+}
